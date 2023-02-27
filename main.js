28a05d650a8e858e778c2664bcba04ecbf4c434d
eart4 <metrika987@gmail.com>
2023-02-27 23:35:15 +0300

    added meadia

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ document.querySelector('.menu-btn').addEventListener('click', function(
 document.querySelector('.header__nav-ul').addEventListener('click', function(
 
     ){
+        document.querySelector('.menu-btn .first').classList.toggle('active')
+        document.querySelector('.menu-btn .second').classList.toggle('active')
+        document.querySelector('.menu-btn .third').classList.toggle('active')
         document.querySelector('nav').classList.toggle('active')
         
         
