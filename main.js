e251f246a8b2855bd7d2287fb2a73bbaff4a5ffd
eart4 <metrika987@gmail.com>
2023-02-27 22:34:44 +0300

    added new sections

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector('.menu-btn').addEventListener('click', function(
+
+){
+    document.querySelector('.menu-btn .first').classList.toggle('active')
+    document.querySelector('.menu-btn .second').classList.toggle('active')
+    document.querySelector('.menu-btn .third').classList.toggle('active')
+    document.querySelector('nav').classList.toggle('active')
+    
+    
+})
+
+
+document.querySelector('.header__nav-ul').addEventListener('click', function(
+
+    ){
+        document.querySelector('nav').classList.toggle('active')
+        
+        
+    })
